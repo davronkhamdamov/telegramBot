f2b6e75622ff3111afa3b2e404922849901ec3a0
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-11-25 22:43:48 +0500

    check with database

--- action/start.js
+++ action/start.js
@@ -1,39 +1,26 @@
-const {bot} = require('../core/bot')
-const {Composer, Markup} = require("telegraf");
-const composer = new Composer()
-const {User} = require('../db/user')
-composer.start(async ctx => {
-  await  ctx.reply(`Xurmatli ${ctx.from.first_name} bizning botimizga xush kelibsiz`, {
+const { bot } = require('../core/bot')
+const { User } = require('../db/user')
+const { isUserActive } = require('./home')
+
+bot.start(async ctx => {
+    const { first_name, id } = ctx.from
+    const getUser = await User.findOne({ user_id: id })
+    if (getUser) {
+        return isUserActive(ctx)
+    }
+    await ctx.reply(`Xurmatli ${first_name} bizning botimizga xush kelibsiz`, {
         reply_markup: {
             keyboard: [
                 [
                     {
-                        text:"Ro'yxatdan o'tish"
+                        text: "Ro'yxatdan o'tish"
                     },
                     {
-                        text:"Adminga yozish ❓"
+                        text: "Adminga yozish ❓"
                     }
                 ]
             ],
             resize_keyboard: true
         }
     })
-    const user = new User({})
 })
-composer.hears('Ro\'yxatdan o\'tish',async ctx=>{
-   await ctx.reply('Ro\'yxatdan o\'tish uchun telefon raqamingizni jo\'nating 📤',{
-        reply_markup:{
-            keyboard: [
-                [
-                    {
-                        text: 'Telefon raqamni jo\'natish 📱',
-                        request_contact: true
-                    }
-                ]
-            ],
-            resize_keyboard: true,
-        }
-    })
-})
-
-bot.use(composer.middleware())
