056209b42b2df286d92996476781c994779ab681
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-11-25 22:39:41 +0500

    settings command added

---
+++ action/settings.js
@@ -0,0 +1,27 @@
+const { bot } = require('../core/bot')
+const { User } = require('../db/user')
+
+bot.command('settings', async ctx => {
+    const user_id = ctx.from.id
+    const user = await User.findOne({ user_id })
+    if (user) {
+        ctx.replyWithHTML(`Hammasi tog'rimi:\n\nFoydalanuvchi ma'lumotlari 👤\nStudent 🎓: ${user.first_name}\nTelefon raqami 📞: +${user.phone_number}\nGuruh 📊: G1\nUsername ${'@' + user.username}`, {
+            reply_markup: {
+                keyboard: [
+                    [
+                        {
+                            text: "O'quvchi ismini o'zgartirish ✏️"
+                        }
+                    ],
+                ],
+                resize_keyboard: true
+            }
+        })
+    } else {
+        ctx.replyWithHTML("Siz ro\'yxatdan o\'tmaganga o\'xshaysiz\nIltimos ro\'yxatdan o\'tish uchun /start ni bosing")
+    }
+})
+
+bot.hears('O\'quvchi ismini o\'zgartirish ✏️', ctx => {
+    ctx.replyWithHTML("Ism Familyangizni kiriting", { reply_markup: { remove_keyboard: true } })
+})
