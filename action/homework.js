c3ba6597d193f49b648b43ddff44dc969c00093e
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-11-25 22:42:23 +0500

    homework added

--- action/homework.js
+++ action/homework.js
@@ -1,19 +1,28 @@
-const {bot} = require('../core/bot')
-const {Composer} = require('telegraf')
-const composer = new Composer()
-const {channel}= require('../config')
-composer.hears('Uyga vazifani topshirish 🗂',async ctx=>{
+const { bot } = require('../core/bot')
+const { channel } = require('../config')
+const { User } = require('../db/user')
+const { isUserActive } = require('./home')
+
+bot.hears('Uyga vazifani topshirish 🗂', async ctx => {
    await ctx.reply("Uyga vazifani tashlash namunasi:")
    await ctx.replyWithDocument('BQACAgIAAx0Cb9TkSAADUmVh1E9U2dBjojDqfcn5a9_-2w-LAAJtOQAC0H4IS8ZdpDwSx-86MwQ',
-       {
-          caption: "Home work 6-lesson"
-       })
-   await bot.on('document',ctx=>{
-         bot.telegram.sendDocument(channel, ctx.message.document.file_id,
-             {
-                caption: ctx.message.document.caption
-             })
+      {
+         caption: "Home work 6-lesson",
+         reply_markup: {
+            keyboard: [
+               [{ text: "🏘 Bosh sahifaga qaytish" }]
+            ],
+            resize_keyboard: true
+         }
+      })
+   bot.hears('🏘 Bosh sahifaga qaytish', ctx => {
+      isUserActive(ctx)
+   })
+   bot.on('document', async ctx => {
+      const user = await User.findOne({ user_id: ctx.from.id })
+      bot.telegram.sendDocument(channel, ctx.message.document.file_id,
+         {
+            caption: user.first_name
+         })
    })
 })
-
-bot.use(composer.middleware())
