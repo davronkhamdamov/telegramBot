b20a0aaa15ef6d8bc6f09a0747930f2f36b2e708
Davron <109721370+davronkhamdamov@users.noreply.github.com>
2023-11-25 23:06:26 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
 require('./core')
 require('./action')
 require('./db')
-
-const express = require('express')
-const app = express()
-const cors = require('cors')
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/', (req, res) => {
-    res.send({
-        message: "it's working"
-    })
-})
-
-app.listen(3000)
