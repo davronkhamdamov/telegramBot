692b0be9d9e7a79e011659120830a095cfba21a4
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-11-25 22:40:58 +0500

    db updated

--- db/user.js
+++ db/user.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    first_name: {type: String, required: true},
-    username: {type: String, required: true},
-    phone_number: {type: String, required: true},
-    user_id: {type: Number, required: true}
+    first_name: { type: String, required: true },
+    username: { type: String, required: true },
+    phone_number: { type: String, required: false },
+    user_id: { type: Number, required: true }
 })
 
 const User = mongoose.model("Users", UserSchema)
