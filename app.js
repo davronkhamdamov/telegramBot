58fedfd5616daa274e035a0efe152d41c775b6ac
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-11-25 22:51:58 +0500

    versel config added

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 require('./core')
 require('./action')
 require('./db')
+
+const http = require('http');
+const cors = require('cors');
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    cors()(req, res, () => {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Hello, this is your Node.js server with CORS enabled!\n');
+    });
+});
+
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
