6fafaf5c0f2b6354b0a7cef88f96f0d9b4beb052
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-11-25 22:40:25 +0500

    ask question command complated

--- action/askquestion.js
+++ action/askquestion.js
@@ -1,12 +1,19 @@
-const {bot} = require('../core/bot')
-const {Composer} = require('telegraf')
-const composer= new Composer()
+const { bot } = require('../core/bot')
+const { isUserActive } = require('./home')
 
-composer.hears('O\'qituvchiga savol berish 👨‍💼️',async ctx=> {
-    ctx.reply('Qanaqa savolingiz bo\'lsa bemalol so\'rayvering men savolingizni o\'qituvchiga yetkazib qo\'yaman')
-    bot.on('text',ctx=>{
-       ctx.replyWithHTML("Savolingiz qabul qilindi tez olada javob olasiz")
+bot.hears('O\'qituvchiga savol berish 👨‍💼️', async ctx => {
+    ctx.reply('Qanaqa savolingiz bo\'lsa bemalol so\'rayvering men savolingizni o\'qituvchiga yetkazib qo\'yaman', {
+        reply_markup: {
+            keyboard: [
+                [{ text: "🏘 Bosh sahifaga qaytish" }]
+            ],
+            resize_keyboard: true
+        }
+    })
+    bot.hears('🏘 Bosh sahifaga qaytish', ctx => {
+        isUserActive(ctx)
+    })
+    bot.on('text', ctx => {
+        ctx.replyWithHTML("Savolingiz qabul qilindi tez olada javob olasiz")
     })
 })
-
-bot.use(composer.middleware())
